5cad5d149cfe24486d5b7ea01a5d6857bf411c59
rep1q <vovasvrn@mail.ru>
2022-10-17 11:57:27 +0300

    Succes Second Task

--- labs_oop/Source/SecondTaskClasses/classesSecondTask.hpp
+++ labs_oop/Source/SecondTaskClasses/classesSecondTask.hpp
@@ -1,13 +1,31 @@
-//
-//  classesSecondTask.hpp
-//  labs_oop
-//
-//  Created by Боб rep1q on 17.10.2022.
-//
-
 #ifndef classesSecondTask_hpp
 #define classesSecondTask_hpp
 
 #include <stdio.h>
+#include <iostream>
 
-#endif /* classesSecondTask_hpp */
+class FuzzyNumber{
+private:
+    double *x;
+    double *e1;
+    double *e2;
+public:
+    FuzzyNumber();
+    FuzzyNumber(double _x,double _e1, double _e2);
+    FuzzyNumber(FuzzyNumber& First);
+    double GetX();
+    double GetE1();
+    double GetE2();
+    const FuzzyNumber operator+(const FuzzyNumber& other)const;
+    const FuzzyNumber operator-(const FuzzyNumber& other)const;
+    const FuzzyNumber operator*(const FuzzyNumber& other)const;
+    const FuzzyNumber operator/(const FuzzyNumber& other)const;
+    const FuzzyNumber& operator=(const FuzzyNumber& other);
+    friend std::ostream& operator<<(std::ostream& out,FuzzyNumber other);
+    friend std::istream& operator>>(std::istream& in,FuzzyNumber& other);
+    friend bool operator==(FuzzyNumber First, FuzzyNumber Second);
+    friend bool operator!=(FuzzyNumber First, FuzzyNumber Second);
+    ~FuzzyNumber();
+};
+
+#endif /* classes_hpp */
