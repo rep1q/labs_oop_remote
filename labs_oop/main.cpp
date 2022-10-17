9bf9cc8f0605193356ee4cf8db71929d44526195
rep1q <vovasvrn@mail.ru>
2022-10-17 11:34:44 +0300

    Initial Commit

---
+++ labs_oop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  labs_oop
+//
+//  Created by Боб rep1q on 17.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
